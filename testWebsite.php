e5595fd1ea9f508cbbc8eafd11cd3b0e2d90e2d1
astrosquid <varcap.cb@gmail.com>
2015-09-10 04:40:36 +0000

    Homework for 9/10

---
+++ testWebsite.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Bootstrap Example Website</title>
+        <!-- Bootstrap -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css"></link>
+
+        
+    </head>
+    <body>
+        <div class="container">
+            <nav class="navbar navbar-default">
+                <ul class="nav navbar-nav">
+                    <li><a class="navbar-brand" href="#">Black Coffee</a></li>
+                    <li><a href="#">For Home</a></li>
+                    <li><a href="#">For Work</a></li>
+                    <li class="nav navbar-nav navbar-right"><button type="button" class="btn btn-danger navbar-btn">Sign in to Coffee</button></li>
+                </ul>
+            </nav>
+            <div class="row">
+               <div class="col-md-12">
+                   <h1>Watch coffee work for you:</h1>
+                   <p>It's good and you know it's good.</p>
+                   <div class="embed-responsive embed-responsive-16by9">
+                       <iframe width="560" height="315" src="https://www.youtube.com/embed/tWCc-p3tFRY" frameborder="0" allowfullscreen></iframe>
+                   </div>
+               </div>
+            
+            
+                <div class="alert alert-danger" role="alert">
+                    <h3>Wow! Just look at all these sultry & tantalizing beans.</h3>
+                </div>
+            
+                <div class="col-xs-6 col-md-4">
+                    <a href="#" class="thumbnail"><img src="http://apcdcl.org/apcdc/images/Coffee-Beans.jpg"></a>
+                </div>
+                <div class="col-xs-6 col-md-4">
+                    <a href="#" class="thumbnail"><img src="https://upload.wikimedia.org/wikipedia/commons/8/87/Coffee_Beans_(2732722806).jpg"></a>
+                </div>
+                <div class="col-xs-12 col-md-4">
+                    <a href="#" class="thumbnail"><img src="http://www.ayurvediclifewithoutpain.com/wp-content/uploads/2012/01/coffee_cup.jpg"></a>
+                </div>
+                <div class="progress-bar col-md-12" role="progressbar" aria-valuenow="50" aria-valuemax="100" style="width: 50%;">
+                    50%
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-12">
+                    <h3>You've visited our site--you're halfway to getting a cup'o'joe! </h3>
+                </div>
+            </div>
+            
+        </div>
+        
+        
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+    </body>    
+    
+</html>
